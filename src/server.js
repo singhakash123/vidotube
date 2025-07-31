59de9090d9699683c560fab5d68ea6edb60c3c07
singhakash123 <akashlife2004@gmail.com>
2025-07-31 09:16:59 +0530

    feat(server): setup dotenv configuration and server port

--- src/server.js
+++ src/server.js
@@ -1,3 +1,26 @@
+import dotenv from "dotenv"
 import { db_connected } from "./db/index.js";
+import { app } from "./app.js";
+
+
+// dotenv config : 
+dotenv.config({
+      path :'./.env'
+})
+
+// port setup : 
+const port = process.env.PORT || 5000
+
+// db connection with server : 
 
 db_connected()
+.then(() =>{
+     app.listen(port , () =>{
+               console.log(`Server connection successfull PORT : ${port}`)
+     })
+})
+.catch((error) =>{
+               console.log(`Server connection failed with database` , error.message);
+
+               process.exit(1)
+})
